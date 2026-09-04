bf8d3e9fe8cba352d95efcd6d65649df042bd742
justynasak <justasak2@gmail.com>
2026-09-04 13:49:32 +0100

    Complete: A teacher has a list of students and their numeric scores. W

--- labs/main.py
+++ labs/main.py
@@ -1,6 +1,18 @@
-def word_lengths(words):
-    lengths = {}
-    # TODO: loop through `words` and populate `lengths` with word -> length of word
-    for word in words:
-        lengths[word] = len(word)
-    return lengths
+def group_by_grade(students):
+    # TODO: determine each student's letter grade and group their names by grade band
+    grades = {}
+    for student in students:
+        score = student["score"]
+
+        if score >= 90:
+            band = "A"
+        elif score >= 80:
+            band = "B"
+        elif score >= 70:
+            band = "C"
+        elif score >= 60:
+            band = "D"
+        else:
+            band = "F"
+        grades.setdefault(band,[]).append(student["name"])
+    return grades
