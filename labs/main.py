704d3d5e03dcf1d6557debc7fa1aa14340dbefc3
justynasak <justasak2@gmail.com>
2026-09-04 13:30:05 +0100

    Complete: Below is a function meant to take a list of words and return

--- labs/main.py
+++ labs/main.py
@@ -1,5 +1,6 @@
-def total_word_count(sentences):
-    count = 0
-    for sentence in sentences:
-        count += len(sentence.split())
-    return count
+def word_lengths(words):
+    lengths = {}
+    # TODO: loop through `words` and populate `lengths` with word -> length of word
+    for word in words:
+        lengths[word] = len(word)
+    return lengths
