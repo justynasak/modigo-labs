5ed419a9042afad9515e1b4708287d9ad8e3fa6f
justynasak <justasak2@gmail.com>
2026-09-17 15:03:36 +0100

    Complete: Arithmetic Expression Evaluator

--- challenges/main.py
+++ challenges/main.py
@@ -1,10 +1,57 @@
-def items_to_restock(current_stock, minimum_levels):
-    # TODO: loop through current_stock, compare against minimum_levels,
-    # and return a list of item names below their minimum
-    restock = []
-    for item,quantity in current_stock.items():
-        if item not in minimum_levels:
-            continue
-        if quantity < minimum_levels[item]:
-            restock.append(item)
-    return restock
+def evaluate(expression):
+    # TODO: parse and evaluate the arithmetic expression yourself, respecting
+    # operator precedence and parentheses. Do not use eval() or exec().
+    tokens = tokenize(expression)
+    pos = [0]
+    
+    def peek():
+        return tokens[pos[0]] if pos[0] < len(tokens) else None
+    
+    def advance():
+        tok = tokens[pos[0]]
+        pos[0] += 1
+        return tok
+
+    def parse_factor():
+        tok = advance()
+        if tok == '(':
+            value = parse_expression()
+            advance()
+            return value
+        return float(tok)
+
+    def parse_term():
+        value = parse_factor()
+        while peek() in ('*','/'):
+            op = advance()
+            rhs = parse_factor()
+            value = value * rhs if op == "*" else value/rhs
+        return value
+
+    def parse_expression():
+        value = parse_term()
+        while peek() in ('+','-'):
+            op = advance()
+            rhs = parse_term()
+            value = value + rhs if op == '+' else value - rhs
+        return value
+
+    return parse_expression()
+
+def tokenize(expression):
+    tokens = []
+    i = 0
+    while i < len(expression):
+        ch = expression[i]
+        if ch == ' ':
+            i += 1
+        elif ch in '+-/*()':
+            tokens.append(ch)
+            i += 1
+        else:
+            j = i
+            while j< len(expression) and expression[j].isdigit():
+                j += 1
+            tokens.append(expression[i:j])
+            i = j
+    return tokens
