2c25c1ce31b7b20c552c4db7cb2f99740b9bf435
justynasak <justasak2@gmail.com>
2026-09-18 08:58:44 +0100

    Complete: Build a Countdown

--- challenges/main.py
+++ challenges/main.py
@@ -1,6 +1,9 @@
-def sum_of_digits(number):
-    # TODO: use a for loop to add up each digit of `number`
-    sum = 0
-    for digit in str(number):
-        sum += int(digit)
-    return sum
+def build_countdown(start):
+    countdown = []
+    # TODO: use a for loop with range() to count down from `start` to 1,
+    # appending each number to `countdown`
+    if not start:
+        return []
+    for n in range(start,0,-1):
+        countdown.append(n)
+    return countdown
