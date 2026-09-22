f3dc10da2bab1d933e559d90348cef656114fee1
justynasak <justasak2@gmail.com>
2026-09-22 12:01:37 +0100

    Complete: Title Case Converter

--- challenges/main.py
+++ challenges/main.py
@@ -1,8 +1,12 @@
-def is_prime(number):
-    # TODO: return True if `number` is prime, False otherwise
-    if number < 2:
-        return False
-    for divisor in range(2,int(number ** 0.5)+1):
-        if number % divisor == 0:
-            return False
-    return True
+def smart_title_case(sentence):
+    # TODO: capitalize each word except connector words (a, an, the, of, in, on, and),
+    # unless that connector word is the first word in the sentence
+    connectors = {"a","an","the","of","in","on","and"}
+    words = sentence.split(" ")
+    result = []
+    for i,word in enumerate(words):
+        if i != 0 and word in connectors:
+            result.append(word)
+        else:
+            result.append(word[0].upper()+word[1:])
+    return " ".join(result)
