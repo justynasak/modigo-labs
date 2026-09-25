151b8e9c2ee124a6eb43635747ade1ab183a5da4
justynasak <justasak2@gmail.com>
2026-09-25 16:31:06 +0100

    Complete: Complete the Code

--- challenges/main.py
+++ challenges/main.py
@@ -1,12 +1,9 @@
-def smart_title_case(sentence):
-    # TODO: capitalize each word except connector words (a, an, the, of, in, on, and),
-    # unless that connector word is the first word in the sentence
-    connectors = {"a","an","the","of","in","on","and"}
-    words = sentence.split(" ")
-    result = []
-    for i,word in enumerate(words):
-        if i != 0 and word in connectors:
-            result.append(word)
-        else:
-            result.append(word[0].upper()+word[1:])
-    return " ".join(result)
+def build_roster(students):
+    roster = {}
+    # TODO: loop through `students` and group names by grade in `roster`
+    if not students:
+        return {}
+    roster = {}
+    for student,grade in students:
+        roster.setdefault(grade,[]).append(student)
+    return roster
